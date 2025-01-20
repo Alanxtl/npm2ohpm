ca91624bc4d44c8b47dfbacdc76a54334cc1035b
Alanxtl <m134679102365478@163.com>
2025-01-20 11:37:12 +0800

    main logic

--- src/parser.py
+++ src/parser.py
@@ -1,23 +1,40 @@
 import os
-from json2json5.json2json5 import convert_json_to_json5
 
-def process_package_json(file_path):
-    print(f'Processing {file_path}')
-    convert_json_to_json5(file_path)
+from src.json2json5.json2json5 import convert_json_to_json5
+from src.js2ets.ts2ets import convert_ts_to_ets
 
-def parse_package(directory):
+def process_package_json(input_file, output_file):
+    print(f'Processing {input_file} to {output_file}')
+    convert_json_to_json5(input_file, output_file)
+
+def process_package_ts(input_file, output_file, client):
+    print(f'Processing {input_file} to {output_file}')
+    convert_ts_to_ets(input_file, output_file, client)
+
+def parse_package(directory, client):
     for root, dirs, files in os.walk(directory):
-        tar_root = root + "converted"
+        tar_root = directory + "-converted"
+        relative_path = os.path.relpath(root, directory)
+
         for file in files:
             if file == 'package.json':
                 full_path = os.path.join(root, file)
-                tar_path = os.path.join(tar_root, file)
+                tar_path = os.path.join(tar_root, relative_path, "oh-package.json5")
+                os.makedirs(os.path.dirname(tar_path), exist_ok=True)
                 process_package_json(full_path, tar_path)
             if file == 'package-lock.json':
                 full_path = os.path.join(root, file)
-                tar_path = os.path.join(tar_root, file)
+                tar_path = os.path.join(tar_root, relative_path, "oh-package.json5")
+                os.makedirs(os.path.dirname(tar_path), exist_ok=True)
                 process_package_json(full_path, tar_path)
-
-if __name__ == "__main__":
-    # 从当前目录开始查找
-    parse_package("C:\\Users\\Alan.lxt-redmi\\Desktop\\1")
+            if file.endswith('.ts'):
+                full_path = os.path.join(root, file)
+                tar_path = os.path.join(tar_root, relative_path, file[:-3] + ".ets")
+                os.makedirs(os.path.dirname(tar_path), exist_ok=True)
+                process_package_ts(full_path, tar_path, client)
+            else:
+                full_path = os.path.join(root, file)
+                tar_path = os.path.join(tar_root, relative_path, file)
+                os.makedirs(os.path.dirname(tar_path), exist_ok=True)
+                with open(full_path, 'rb') as fsrc, open(tar_path, 'wb') as fdst:
+                    fdst.write(fsrc.read())
